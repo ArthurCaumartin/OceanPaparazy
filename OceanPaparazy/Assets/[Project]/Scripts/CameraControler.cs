4369000ecbd53bf9ef72aabb7bdd2004bf756102
Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
2025-02-25 15:58:08 +0100

    $oiuytrsqwxcfghjkop

--- OceanPaparazy/Assets/[Project]/Scripts/CameraControler.cs
+++ OceanPaparazy/Assets/[Project]/Scripts/CameraControler.cs
@@ -3,23 +3,50 @@ using UnityEngine.InputSystem;
 
 public class CameraControler : MonoBehaviour
 {
+    [SerializeField] private Transform _target;
+
+    [Header("Third Persone Parameter : ")]
+    [SerializeField] private float _followSpeed = 5;
+    [SerializeField] private float _followDistance = 5;
+
+    [Header("First Persone Parameter : ")]
     [SerializeField] private float _acceleration = 5;
     [SerializeField] private float _speed = 5;
     private Vector2 _inputVector;
     private Vector3 _lookRotation;
+    private bool _isFirstPersone = false;
+
 
     private void Update()
     {
+        if (_isFirstPersone)
+            LookAround();
+        else
+            FollowTarget();
+    }
+
+    private void FollowTarget()
+    {
+        Vector3 posTarget = new Vector3(transform.position.x, transform.position.y, transform.position.z - _followDistance);
+        transform.position = Vector3.Lerp(transform.position, posTarget, Time.deltaTime * _followSpeed);
+        transform.LookAt(_target);
+    }
+
+    private void LookAround()
+    {
         _lookRotation.x += -_inputVector.y * _speed;
         _lookRotation.x = Mathf.Clamp(_lookRotation.x, -90, 90);
-
         _lookRotation.y += _inputVector.x * _speed;
-
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(_lookRotation), Time.deltaTime * _acceleration);
     }
 
     private void OnLook(InputValue value)
     {
         _inputVector = value.Get<Vector2>();
+    }
+
+    public void EnableFirstPersone(bool value)
+    {
+        _isFirstPersone = value;
     }
 }
--- OceanPaparazy/Assets/[Project]/Scripts/PictureTakerControler.cs
+++ OceanPaparazy/Assets/[Project]/Scripts/PictureTakerControler.cs
@@ -20,6 +20,7 @@ public class PictureTakerControler : MonoBehaviour
 
     void Update()
     {
+        // BurstRay();
     }
 
     private void OnShoot(InputValue value)
@@ -28,46 +29,51 @@ public class PictureTakerControler : MonoBehaviour
         if (value.Get<float>() > .5f)
         {
             TakePicture();
-            BurstRay();
+            // BurstRay();
         }
     }
 
-    public void BurstRay()
-    {
-        Texture2D tex2D = new Texture2D((int)(1 / _rayResolutionX), (int)(1 / _rayResolutionY));
-        for (float x = 0; x < 1; x += _rayResolutionX)
-        {
-            for (float y = 0; y < 1; y += _rayResolutionY)
-            {
-                Vector2 pixelPos;
-                pixelPos.x = Mathf.Lerp(0, _camera.pixelWidth, x);
-                pixelPos.y = Mathf.Lerp(0, _camera.pixelHeight, y);
-                Ray r = _camera.ScreenPointToRay(pixelPos);
-                Physics.Raycast(r, out RaycastHit hit, _range);
+    // public Transform[] GetEntity()
+    // {
+    //     return null;
+    // }
 
-                if (hit.collider)
-                {
-                    Debug.DrawLine(hit.point + Vector3.down, hit.point + Vector3.up, Color.green);
-                    // float depth = Vector3.Distance(_camera.transform.position, hit.point);
-                    // depth = Mathf.InverseLerp(0, 150, depth);
-                    tex2D.SetPixel((int)Mathf.Lerp(0, tex2D.width, x)
-                                    , (int)Mathf.Lerp(0, tex2D.height, y)
-                                    // , Color.Lerp(Color.cyan, Color.black, depth));
-                                    , Color.cyan);
-                }
-                else
-                {
-                    tex2D.SetPixel((int)Mathf.Lerp(0, tex2D.width, x), (int)Mathf.Lerp(0, tex2D.height, y), Color.black);
-                }
+    // public void BurstRay(Transform)
+    // {
+    //     Texture2D tex2D = new Texture2D((int)(1 / _rayResolutionX), (int)(1 / _rayResolutionY));
+    //     for (float x = 0; x < 1; x += _rayResolutionX)
+    //     {
+    //         for (float y = 0; y < 1; y += _rayResolutionY)
+    //         {
+    //             Vector2 pixelPos;
+    //             pixelPos.x = Mathf.Lerp(0, _camera.pixelWidth, x);
+    //             pixelPos.y = Mathf.Lerp(0, _camera.pixelHeight, y);
+    //             Ray r = _camera.ScreenPointToRay(pixelPos);
+    //             Physics.Raycast(r, out RaycastHit hit, _range);
 
-                Debug.DrawRay(r.origin, r.direction * _range, new Color(1, 0, 0, .4f));
+    //             if (hit.collider)
+    //             {
+    //                 Debug.DrawLine(hit.point + Vector3.down, hit.point + Vector3.up, Color.green);
+    //                 // float depth = Vector3.Distance(_camera.transform.position, hit.point);
+    //                 // depth = Mathf.InverseLerp(0, 150, depth);
+    //                 tex2D.SetPixel((int)Mathf.Lerp(0, tex2D.width, x)
+    //                                 , (int)Mathf.Lerp(0, tex2D.height, y)
+    //                                 // , Color.Lerp(Color.cyan, Color.black, depth));
+    //                                 , Color.cyan);
+    //             }
+    //             else
+    //             {
+    //                 tex2D.SetPixel((int)Mathf.Lerp(0, tex2D.width, x), (int)Mathf.Lerp(0, tex2D.height, y), Color.black);
+    //             }
 
-            }
-        }
+    //             Debug.DrawRay(r.origin, r.direction * _range, new Color(1, 0, 0, .4f));
 
-        tex2D.Apply();
-        SavePNG("RayTexture", tex2D);
-    }
+    //         }
+    //     }
+
+    //     tex2D.Apply();
+    //     SavePNG("RayTexture", tex2D);
+    // }
 
     public void TakePicture()
     {
@@ -92,11 +98,16 @@ public class PictureTakerControler : MonoBehaviour
     public void SavePNG(string pngName, Texture2D tex)
     {
         byte[] bytes = tex.EncodeToPNG();
-        string filePath = Path.Combine(Application.dataPath + "/[Project]/Photos", $"{pngName}_{_count}.png");
-        File.WriteAllBytes(filePath, bytes);
-        if (Application.isEditor)
-            AssetDatabase.Refresh();
+        string filePath = Application.dataPath + "/[Project]/Photos";
+        string fileName = $"/{pngName}_{_count}.png";
 
+        File.WriteAllBytes(filePath + fileName, bytes);
+
+        // StreamWriter test = File.AppendText(filePath + "/picture.txt");
+        // test.WriteLine($"/{pngName}_{_count}.png");
+        // test.Close();
+
+        if (Application.isEditor) AssetDatabase.Refresh();
         _count++;
     }
 }
