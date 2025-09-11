8f8fab6a6e5a44fd4ca15b600da0a8ab2f5fe00f
Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
2025-09-11 05:44:27 +0200

    State parent class : add init fct + base constructor

--- OceanPaparazy/Assets/[Project]/Scripts/StateMachine/State.cs
+++ OceanPaparazy/Assets/[Project]/Scripts/StateMachine/State.cs
@@ -4,11 +4,18 @@ using System;
 public abstract class State
 {
     protected StateMachine _stateMachine;
+
+    public virtual void Init(StateMachine stateMachine)
+    {
+        _stateMachine = stateMachine;
+    }
+
     public virtual void EnterState() { }
     public virtual void UpdateState() { }
     public virtual void FixedUpdateState() { }
     public virtual void ExitState() { }
 
+    public State() { }
     public State(StateMachine stateMachine = null)
     {
         _stateMachine = stateMachine;
